5625107d05856266384e799e15fe520a9ece4b91
Carlos Rubiales Bravo <alumno@viralgamingacademy.com>
2019-06-21 21:01:08 +0200

    Updated

---
+++ Assets/_CursoIMS-3DUnity/Scripts/Utils/KeyBindingToTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class KeyBinding
+{
+  public KeyCode key;
+  public string triggerName;
+}
+
+[RequireComponent(typeof(Animator))]
+public class KeyBindingToTrigger : MonoBehaviour
+{
+
+  public List<KeyBinding> keyBindings;
+
+
+  private Animator animator;
+
+
+  public void Awake()
+  {
+    animator = GetComponent<Animator>();
+
+    if (animator == null)
+    {
+      enabled = false;
+    }
+  }
+  
+  void Update()
+  {
+    foreach (KeyBinding kb in keyBindings)
+    {
+      if (Input.GetKeyDown(kb.key))
+      {
+        animator.SetTrigger(kb.triggerName);
+      }
+    }
+  }
+}
