e2f829f8f441c48f5775f8106c74f7e7c45f2b63
VGA <alumno@viralstudios.es>
2019-06-27 14:29:49 +0200

    GitHUb final

---
+++ Assets/_Curso-3DUnity/Scripts/AI/MonsterStep.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterStep : MonoBehaviour
+{
+  void FootStep()
+  {
+    Debug.Log("Paso del monstruo");
+  }
+}
